fbf94031294505051aa073e4b0af3b050482fe6b
Theodoros Konstantopoulos <tkonstantopoulos@wpi.edu>
2019-05-12 22:59:23 -0400

    home screen front end

--- app/src/main/java/com/kallendr/android/ui/home/MainActivity.java
+++ app/src/main/java/com/kallendr/android/ui/home/MainActivity.java
@@ -2,6 +2,9 @@ package com.kallendr.android.ui.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.kallendr.android.R;
 
@@ -10,6 +13,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
         setContentView(R.layout.activity_main);
     }
+
+    public void btn_createTeam(View view) {
+
+    }
+
+    public void btn_joinTeam(View view) {
+
+    }
+
 }
