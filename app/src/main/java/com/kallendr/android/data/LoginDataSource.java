bd8b37fd0320d9405586f3bf490abb25d0451107
Theodoros Konstantopoulos <tkonstantopoulos@wpi.edu>
2019-05-27 19:07:06 -0400

    Linked firebase with app and implemented firebase auth for logging in

--- app/src/main/java/com/kallendr/android/data/LoginDataSource.java
+++ app/src/main/java/com/kallendr/android/data/LoginDataSource.java
@@ -1,5 +1,8 @@
 package com.kallendr.android.data;
 
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.kallendr.android.data.model.LoggedInUser;
 
 import java.io.IOException;
@@ -11,19 +14,20 @@ public class LoginDataSource {
 
     public Result<LoggedInUser> login(String username, String password) {
 
-        try {
-            // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
-        } catch (Exception e) {
-            return new Result.Error(new IOException("Error logging in", e));
+        AuthResult authResult = FirebaseAuth.getInstance().signInWithEmailAndPassword(username, password)
+                .getResult();
+
+        if (authResult != null) {
+            FirebaseUser authUser = authResult.getUser();
+
+            return new Result.Success<>(authUser);
+        } else {
+            return new Result.Error(new IOException("Error logging in"));
         }
+
     }
 
     public void logout() {
-        // TODO: revoke authentication
+        FirebaseAuth.getInstance().signOut();
     }
 }
--- app/src/main/java/com/kallendr/android/ui/home/MainActivity.java
+++ app/src/main/java/com/kallendr/android/ui/home/MainActivity.java
@@ -1,28 +1,42 @@
 package com.kallendr.android.ui.home;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.kallendr.android.R;
+import com.kallendr.android.ui.calendar.MyCalendar;
 import com.kallendr.android.ui.login.LoginActivity;
 import com.kallendr.android.ui.register.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        try
-        {
-            this.getSupportActionBar().hide();
-        }
-        catch (NullPointerException e){}
         setContentView(R.layout.activity_main);
+
+        // Check if user has already signed in
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user != null) {
+                    Intent intent = new Intent(MainActivity.this, MyCalendar.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
     }
 
     public void btn_createTeam(View view) {
