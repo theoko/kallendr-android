bd2904bd1da88c8df72af33bca088228b8727a37
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2019-09-26 13:38:16 -0400

    UI improvements

--- app/src/main/java/com/kallendr/android/ui/calendar/CalendarActivity.java
+++ app/src/main/java/com/kallendr/android/ui/calendar/CalendarActivity.java
@@ -93,7 +93,7 @@ public class CalendarActivity extends AppCompatActivity {
         btnScheduleMeeting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                
             }
         });
 
---
+++ app/src/main/java/com/kallendr/android/ui/calendar/NewMeetingScheduleActivity.java
@@ -0,0 +1,15 @@
+package com.kallendr.android.ui.calendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kallendr.android.R;
+
+public class NewMeetingScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_meeting_schedule);
+    }
+}
